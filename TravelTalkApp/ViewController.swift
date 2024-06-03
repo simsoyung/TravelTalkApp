74b5c74bc3fd6ead6cdbf943e948d4d2e69ac091
soyung sim <simsy1024@gmail.com>
2024-06-03 16:23:51 +0900

    Initial Commit

---
+++ TravelTalkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelTalkApp
+//
+//  Created by 심소영 on 6/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
